aabdd1008e0b769f8ee5a5bf34ed79a550015618
itsjon4s <motajoao670@gmail.com>
2022-08-26 20:09:54 +0100

    fix: wrong nodes example

--- nodes.example.ts
+++ nodes.example.ts
@@ -1,12 +1,10 @@
 import { NodeOptions } from 'vulkava';
 
-export default {
-  nodes: [
-    {
-      id: '01',
-      hostname: 'localhost',
-      port: 443,
-      password: 'youshallnotpass'
-    }
-  ] as NodeOptions[]
-};
+export const Nodes = [
+  {
+    id: 'Node-01',
+    hostname: 'localhost',
+    port: 443,
+    password: 'youshallnotpass'
+  }
+] as NodeOptions[];
